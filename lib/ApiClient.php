e7a0f99996d9a518807e3b20d5709a637a8e500c
Pablo Siciliano <pablo.siciliano@gmail.com>
2023-01-04 17:32:44 -0300

    Update ApiClient.php
    Changes to make it compatible with PHP 8.2

--- lib/ApiClient.php
+++ lib/ApiClient.php
@@ -4,4 +4,23 @@ namespace MailchimpMarketing;
 
 use MailchimpMarketing\Configuration;
 
-class ApiClient extends Configuration {}
+class ApiClient extends Configuration {
+
+  public function __set($name,$value) {
+      
+    if(property_exists($this,$name))
+    {
+      $this->$name=$value;
+    }
+  }
+  
+  public function __get($name) {
+      
+    if(property_exists($this,$name))
+    {
+      return $this->$name;
+    }
+    return null;
+  }
+
+}
